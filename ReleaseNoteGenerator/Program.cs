46c0dd7fd65fe364aa9be176908203a50e52bc2b
verlox <verlox@riseup.net>
2022-12-23 20:45:24 -0500

    add inputs and writing to console

--- ReleaseNoteGenerator/Program.cs
+++ ReleaseNoteGenerator/Program.cs
@@ -2,26 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using Veylib.Utilities.Net;
+using Veylib.ICLI;
 using System.Net;
 using System.IO;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Net.Configuration;
 using System.Security.Policy;
+using System.Drawing;
+using Veylib;
 
 namespace ReleaseNoteGenerator
 {
     internal class Program
     {
+        static Core core = new Core();
         static Regex keywordsRegex = new Regex(@"add|update|change|fix|remove|pull request|rename|cleanup|format");
         static Regex swearingRegex = new Regex(@"([a-z0-9]{0,99})(fuck|shit|ass)([a-z0-9]{0,99})", RegexOptions.IgnoreCase);
         static List<string> swearingWhitelist = new List<string> { "class", "password" };
-        static void Main(string[] args)
-        {
-            string sinceSha = "a0cd691637d94e50421f78990484f05283053c6d";
-            Dictionary<string, List<string>> messages = new Dictionary<string, List<string>>
+        static Dictionary<string, List<string>> messages = new Dictionary<string, List<string>>
             {
                 {
                     "add",
@@ -65,22 +66,118 @@ namespace ReleaseNoteGenerator
                 }
             };
 
+        static void ratelimitCheck(NetResponse response)
+        {
+            if (response.Headers.GetValues("X-Ratelimit-Remaining")[0] == "0")
+            {
+                core.WriteLine(Color.Red, $"Ratelimited until {General.FromEpoch(long.Parse(response.Headers.GetValues("X-Ratelimit-Reset")[0]))}, press any key to exit");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Core.StartupProperties properties = new Core.StartupProperties
+            {
+                Author = new Core.StartupAuthorProperties
+                {
+                    Name = "verlox",
+                    Url = "verlox.cc"
+                },
+                Version = "1.0.0.0",
+                Title = new Core.StartupConsoleTitleProperties
+                {
+                    Animated = false,
+                    Text = "CommitCompiler"
+                },
+                Logo = new Core.StartupLogoProperties
+                {
+                    AutoCenter = true,
+                    Text = @"
+_________                           ___________________                        ___________            
+__  ____/____________ __________ ______(_)_  /__  ____/____________ ______________(_)__  /____________
+_  /    _  __ \_  __ `__ \_  __ `__ \_  /_  __/  /    _  __ \_  __ `__ \__  __ \_  /__  /_  _ \_  ___/
+/ /___  / /_/ /  / / / / /  / / / / /  / / /_ / /___  / /_/ /  / / / / /_  /_/ /  / _  / /  __/  /    
+\____/  \____//_/ /_/ /_//_/ /_/ /_//_/  \__/ \____/  \____//_/ /_/ /_/_  .___//_/  /_/  \___//_/     
+                                                                       /_/                            "
+                },
+                DefaultMessageLabel = null,
+                DefaultMessageTime = null
+            };
+            core.Start(properties);
+
+            Regex shaRegex = new Regex(@"^[a-z0-9]{40}$", RegexOptions.IgnoreCase);
+            Regex userRepoRegex = new Regex(@"^([a-z0-9\-]{1,39}/[a-z0-9\-\.]{1,100})$", RegexOptions.IgnoreCase);
+
+        repoInput:
+            string repo = "";
+
+            string userrepo = core.ReadLine("Enter repository url $ ", Color.White);
+            userrepo = userrepo.Replace("https://github.com/", "");
+            Match userRepoMatch = userRepoRegex.Match(userrepo);
+            if (!userRepoMatch.Success)
+                goto repoInput;
+
+            repo = userRepoMatch.Groups[1].Value;
+
+            NetRequest request = new NetRequest($"https://api.github.com/repos/{repo}");
+            request.SetHeader("User-Agent", "CommitCompiler");
+            NetResponse response = request.Send();
+
+            if (response.Status != HttpStatusCode.OK)
+            {
+                ratelimitCheck(response);
+                core.WriteLine(Color.Red, "Repo does not exist");
+                goto repoInput;
+            }
+        shaInput:
+            string sinceSha = core.ReadLine("Since commit (SHA hash) $ ", Color.White);
+
+            if (shaRegex.Matches(sinceSha).Count != 1)
+                goto shaInput;
+
+            request = new NetRequest($"https://api.github.com/repos/{repo}/commits/{sinceSha}");
+            request.SetHeader("User-Agent", "CommitCompiler");
+            response = request.Send();
+
+            if (response.Status != HttpStatusCode.OK)
+            {
+                ratelimitCheck(response);
+                core.WriteLine(Color.Red, "Commit does not exist");
+                goto shaInput;
+            }
+
+            retrieveCommits:
+
             int page = 1;
             int totalCommits = 0;
             try
             {
                 bool foundSha = false;
+                core.WriteLine("Started scanning for commits in repository ", Color.White, repo, "...");
                 while (!foundSha)
                 {
-                    NetRequest request = new NetRequest($"https://api.github.com/repos/verlox/Veylib/commits?page={page}&per_page=100");
+                    request = new NetRequest($"https://api.github.com/repos/{repo}/commits?page={page}&per_page=100");
                     request.SetHeader("User-Agent", "CommitCompiler");
-                    NetResponse response = request.Send();
+                    response = request.Send();
                     dynamic json = response.ToJson();
                     if (response.Status != HttpStatusCode.OK)
-                        throw new Exception(response.Content);
+                    {
+                        ratelimitCheck(response);
+                        core.WriteLine(Color.Red, $"Failed to send request: {response.Content}, retry?");
+                        switch (new SelectionMenu("Retry", "Exit").Activate())
+                        {
+                            case "Retry":
+                                goto retrieveCommits;
+                            case "Exit":
+                                Environment.Exit(0);
+                                return;
+                        }
+                    }
                     else if (json?.Count == 0 || json == null)
                         break;
 
+                    core.WriteLine("Reading page ", Color.White, page.ToString(), null, ", total commits on page: ", Color.White, json.Count.ToString());
                     foreach (dynamic commit in json)
                     {
                         totalCommits++;
@@ -98,6 +195,7 @@ namespace ReleaseNoteGenerator
                         list.Add($"* [{commit.sha}] {message[0].ToString().ToUpper()}{message.Substring(1, message.Length - 1).Split('\n')[0]}");
                         if (commit.sha == sinceSha)
                         {
+                            core.WriteLine("Found commit hash that matches original (", Color.White, sinceSha, null, "), total commits logged and sorted: ", Color.White, totalCommits.ToString());
                             foundSha = true;
                             break;
                         }
@@ -113,6 +211,7 @@ namespace ReleaseNoteGenerator
             if (File.Exists("commits.txt"))
                 File.Delete("commits.txt");
 
+            core.WriteLine("Compiling into commits.txt");
             foreach (var msg in messages)
             {
                 if (msg.Value.Count == 0)
@@ -139,7 +238,19 @@ namespace ReleaseNoteGenerator
                 }
             }
 
-            File.AppendAllText("commits.txt", $"\n# Statistics\n\n* **Commits done**: {totalCommits}\n\n(Generated with *CommitCompiler* made by **verlox**)");
+            File.AppendAllText("commits.txt", $"\n# Statistics\n\n* **Commits done**: {totalCommits}\n\n(Generated with [*CommitCompiler*](https://github.com/verlox/CommitCompiler) made by **verlox**)");
+
+            core.WriteLine(Color.LimeGreen, "Finished outputting to ", Color.Lime, "commits.txt", Color.LimeGreen, ", open the file?");
+            if (new SelectionMenu("Yes", "No").Activate() == "Yes")
+            {
+                Process.Start("commits.txt");
+                core.WriteLine("Closing in 5 seconds...");
+                new Thread(() =>
+                {
+                    Thread.Sleep(5000);
+                    Environment.Exit(0);
+                }).Start();
+            }
         }
     }
 }
